aa72257af4f4787bfc8ebc6b05f69129110f7b50
libreally <2359304261@qq.com>
2022-11-25 15:09:21 +0800

    v4

--- csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
+++ csmall-stock/csmall-stock-webapi/src/main/java/cn/tedu/csmall/stock/webapi/controller/StockController.java
@@ -1,8 +1,12 @@
 package cn.tedu.csmall.stock.webapi.controller;
 
+import cn.tedu.csmall.commons.exception.CoolSharkServiceException;
 import cn.tedu.csmall.commons.pojo.stock.dto.StockReduceCountDTO;
 import cn.tedu.csmall.commons.restful.JsonResult;
+import cn.tedu.csmall.commons.restful.ResponseCode;
 import cn.tedu.csmall.stock.service.IStockService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +30,46 @@ public class StockController {
 
     @PostMapping("/reduce/count")
     @ApiOperation("减少库存数")
+    @SentinelResource(value = "减少库存数",blockHandler = "blockError",fallback = "fallbackError")
     public JsonResult reduceCommodityCount(StockReduceCountDTO stockReduceCountDTO){
+        // 调用业务逻辑层
         stockService.reduceCommodityCount(stockReduceCountDTO);
-        return JsonResult.ok("库存减少完成");
+        // 随机发生异常,测试自定义降级方法效果
+        if(Math.random()<0.5){
+            throw new CoolSharkServiceException(
+                    ResponseCode.INTERNAL_SERVER_ERROR,"发生随机异常");
+        }
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return JsonResult.ok("库存减少完成!");
     }
+
+    // Sentinel自定义限流方法定义规则
+    // 1.访问修饰符必须是public
+    // 2.返回值类型必须和控制器方法一致
+    // 3.方法名必须是控制器方法注解中由blockHandler标记的方法名称
+    // 4.方法的参数必须包含控制器方法的所有参数,再额外添加一个BlockException的异常参数类型
+    public JsonResult blockError(StockReduceCountDTO stockReduceCountDTO,
+                                 BlockException e){
+        // 这个方法运行表示当前请求被限流了,我们给与返回,提示它被限流即可
+        return JsonResult.failed(
+                ResponseCode.INTERNAL_SERVER_ERROR,"服务器忙,请稍后再试");
+    }
+
+    // 自定义降级方法: 由@SentinelResource的fallback属性指定
+    // 自定义降级方法和自定义限流方法的格式规则基本一致
+    // 只是额外返回的异常类型使用Throwable
+    // 当上面的控制层方法发生异常时,会调用自定义降级方法
+    // 实际开发中,可能调用运行一些老版本的代码,所以称之为"降级"
+    public JsonResult fallbackError(StockReduceCountDTO stockReduceCountDTO,
+                                    Throwable throwable){
+        // 输入异常信息
+        throwable.printStackTrace();
+        // 返回降级方法运行的信息
+        return JsonResult.failed(ResponseCode.INTERNAL_SERVER_ERROR,"服务降级");
+    }
+
 }
